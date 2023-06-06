37ef5c71d77d2241229a8c5f3137596e9ece851c
angelsflyinhell <azami@namespace.media>
2023-06-06 18:04:32 +0200

    Update debug.rs

--- src/api/debug.rs
+++ src/api/debug.rs
@@ -1,17 +1,9 @@
 use actix_web::{
     get,
-    web::{Data, Json},
+    web::{Json},
 };
-use sqlite::Connection;
 
 #[get("/")]
 pub async fn index() -> Json<String> {
-    Json("Barebones, anonymous chat platform as a web server using SQLite and Rust.\nLearn more: https://github.com/angelsflyinhell/ping".to_string())
-}
-
-#[get("/dbg/insert_sample_data")]
-pub async fn insert_sample_data(db: Data<Connection>) -> Json<String> {
-    db.execute("INSERT INTO messages (content) VALUES ('Hello, world!');")
-        .unwrap();
-    Json("Inserted sample data.".to_string())
+    Json("Barebones, anonymous chat platform as a web server using SQLite and Rust. Learn more: https://github.com/angelsflyinhell/ping".to_string())
 }
