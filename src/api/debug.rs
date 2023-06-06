eeaac2badc6bbdff10807f51b21bc490f34e54f4
yuki <40737538+angelsflyinhell@users.noreply.github.com>
2023-06-06 12:30:51 +0200

    Update debug.rs

--- src/api/debug.rs
+++ src/api/debug.rs
@@ -2,5 +2,5 @@ use actix_web::{get, Responder, HttpResponse};
 
 #[get("/")]
 pub async fn index() -> impl Responder {
-    HttpResponse::Ok().body("Learn more: https://github.com/angelsflyinhell/ping")
+    HttpResponse::Ok().body("Barebones, anonymous chat platform as a web server using SQLite and Rust.\nLearn more: https://github.com/angelsflyinhell/ping")
 }
