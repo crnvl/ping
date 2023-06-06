bd09ef77a9e918d95181c34c26b4a601b9ff2db5
angelsflyinhell <azami@namespace.media>
2023-06-06 16:13:42 +0200

    Update messages.rs

--- src/api/messages.rs
+++ src/api/messages.rs
@@ -1,18 +1,19 @@
 use actix_web::{
     get,
-    web::{Data, Json},
+    web::{Data, Json, self},
 };
 use sqlite::{Connection, State};
 
 use crate::models::Message;
 
 #[get("/posts/{board}")]
-pub async fn get_posts(db: Data<Connection>) -> Json<Vec<Message>> {
-    let query = "SELECT * FROM messages WHERE board = 'all'";
+pub async fn get_posts(path: web::Path<String>,db: Data<Connection>) -> Json<Vec<Message>> {
+    let board = path.into_inner();
+
+    let query = format!("SELECT * FROM messages WHERE board = '{}'", board);
     let mut statement = db.prepare(query).unwrap();
 
     let mut posts = Vec::new();
-
     while let State::Row = statement.next().unwrap() {
         posts.push(Message {
             board: statement.read::<String, _>(0).unwrap(),
