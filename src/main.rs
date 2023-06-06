91063b6eae725f1d48e2586dd94c2fd16e079b39
yuki <40737538+angelsflyinhell@users.noreply.github.com>
2023-06-06 11:19:13 +0200

    Config web server

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,28 @@
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 use std::path::Path;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("SQLite test!");
-
-    let mut create_tables = "";
-    if !Path::new("main.db").exists() {
-        create_tables = "CREATE TABLE messages (content TEXT, username TEXT DEFAULT 'anonymous', time DATETIME DEFAULT CURRENT_TIMESTAMP);"
-    }
-
-    let connection = sqlite::open("main.db").unwrap();
-    connection.execute(create_tables).unwrap();
-
     std::env::set_var("RUST_LOG", "debug");
     std::env::set_var("RUST_BACKTRACE", "1");
+    env_logger::init();
 
-    HttpServer::new(|| App::new().app_data(connection))
-        .bind(("127.0.0.1", 80))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let mut create_tables = "";
+        if !Path::new("main.db").exists() {
+            create_tables = "CREATE TABLE messages (content TEXT, username TEXT DEFAULT 'anonymous', time DATETIME DEFAULT CURRENT_TIMESTAMP);"
+        }
+
+        let connection = sqlite::open("main.db").unwrap();
+        connection.execute(create_tables).unwrap();
+
+        let data = Data::new(connection);
+
+        let logger = Logger::default();
+        App::new().wrap(logger)
+        .app_data(data)
+    })
+    .bind(("0.0.0.0", 80))?
+    .run()
+    .await
 }
