d278bea639df44280adfebfa984809427aa4776f
angelsflyinhell <azami@namespace.media>
2023-06-07 17:45:32 +0200

    Handle invalid message IDs on message specific request

--- src/api/messages.rs
+++ src/api/messages.rs
@@ -41,14 +41,25 @@ pub async fn get_post(path: web::Path<i64>, db: Data<Connection>) -> Json<Messag
 
     statement.next().unwrap();
 
-    let post = Message {
-        id: statement.read::<i64, _>(0).unwrap(),
-        board: statement.read::<String, _>(1).unwrap(),
-        thumb_url: statement.read::<String, _>(2).unwrap(),
-        content: statement.read::<String, _>(3).unwrap(),
-        username: statement.read::<String, _>(4).unwrap(),
-        ref_id: statement.read::<i64, _>(5).unwrap(),
-        time: statement.read::<String, _>(6).unwrap(),
+    let post = match statement.read::<String, _>(3) {
+        Ok(_) => Message {
+            id: statement.read::<i64, _>(0).unwrap(),
+            board: statement.read::<String, _>(1).unwrap(),
+            thumb_url: statement.read::<String, _>(2).unwrap(),
+            content: statement.read::<String, _>(3).unwrap(),
+            username: statement.read::<String, _>(4).unwrap(),
+            ref_id: statement.read::<i64, _>(5).unwrap(),
+            time: statement.read::<String, _>(6).unwrap(),
+        },
+        Err(_) => return Json(Message {
+            id: 0,
+            board: "".to_string(),
+            thumb_url: "".to_string(),
+            content: "Post not found.".to_string(),
+            username: "".to_string(),
+            ref_id: 0,
+            time: "".to_string(),
+        }),
     };
 
     Json(post)
